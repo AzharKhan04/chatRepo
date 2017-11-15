6d76fb204e87b32efee288d4a185d557b9ada732
chandankrishnan <chandan.mandre10@gmail.com>
2017-11-15 15:12:37 +0530

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-//config
-var config = {
-    "accessKeyId": "AKIAI3JN4OWRODJANZZQ",
-    "secretAccessKey": "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",
-    "region": "us-east-1",
-    "bucket":"invoicehipcask"
-}
-module.exports = config
