2b9508745f77d74c6cad351bcf092643a7ca30eb
chandan <chandan@hipcask.com>
2017-11-15 15:23:20 +0530

    err fix

---
+++ config.js
@@ -0,0 +1,7 @@
+"use strict";	
+var config = {		  
+  "accessKeyId": "AKIAI3JN4OWRODJANZZQ",
+  "secretAccessKey": "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",
+    "region": "us-east-1",		      
+    "bucket":"invoicehipcask"		
+}		  
